0e923a1cf8189b69be60f78035ac5863adff65f8
root <root@DESKTOP-7SKTBOM.localdomain>
2019-12-16 21:26:57 -0500

    instance updated

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web12" { 
-  ami           = "ami-00068cd7555f543d5" 
+  ami           = "ami-0b2d8d1abb76a53d8" 
   instance_type = "t2.micro" 
   
-  key_name = "aws_key_pair.deployer"
+  key_name = "aws_key_pair.deployer.key_name"
 
 tags = { 
     Name = "HelloWorld" 
