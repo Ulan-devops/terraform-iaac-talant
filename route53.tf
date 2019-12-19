dfe07ac10fbd8861d11659b2559a601bb8e8ffc5
root <root@DESKTOP-7SKTBOM.localdomain>
2019-12-18 20:31:39 -0500

    route53 updated

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "www" { 
-  zone_id = "ZFM6PTLZKE7ET" 
-  name    = "www.talantzon.com" 
-  type    = "A" 
-  ttl     = "60" 
-  records = [aws_instace.web12.public_ip] 
-} 
+resource "aws_route53_record" "www" {
+  zone_id = "ZFM6PTLZKE7ET"
+  name    = "www.talantzon.com"
+  type    = "A"
+  ttl     = "60"
+  records = [aws_instance.web12.public_ip]
+}
