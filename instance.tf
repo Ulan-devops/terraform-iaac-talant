00e64050c1e1e5827dee2a8c791c6b27fb121ca0
ktalant <uchkunfan@gmail.com>
2019-12-21 12:29:34 -0500

    added tfvars

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web12" { 
   count     = 2
-  ami           = "ami-0b2d8d1abb76a53d8" 
+  ami           = var.ami
   instance_type = "t2.micro" 
   
   key_name = aws_key_pair.deployer.key_name
