1da458d36e0f47d9d38a313b5e9e8955d1cd9bfc
root <root@DESKTOP-7SKTBOM.localdomain>
2019-12-21 11:27:53 -0500

    updated

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web12" { 
-  count = 2
+  
   ami           = "ami-0b2d8d1abb76a53d8" 
   instance_type = "t2.micro" 
   
@@ -9,7 +9,7 @@ resource "aws_instance" "web12" {
 
 
   tags = { 
-    Name = "HelloWorld${count.index +1}"
+    Name = "HelloWorld"
   }
   lifecycle {
     prevent_destroy = true
