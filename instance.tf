cb6a7c8b3b866301b6f7666cabee91fc7c9e3682
ktalant <uchkunfan@gmail.com>
2019-12-21 11:58:03 -0500

    upated

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web12" { 
-  
+  count     = 2
   ami           = "ami-0b2d8d1abb76a53d8" 
   instance_type = "t2.micro" 
   
@@ -9,7 +9,7 @@ resource "aws_instance" "web12" {
 
 
   tags = { 
-    Name = "HelloWorld"
+    Name = "HelloWorld"$(count +1)
   }
 /*   lifecycle {
     prevent_destroy = true
