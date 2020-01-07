609549fee6664d86a39ba994b599f3ff4575a55e
ktalant <uchkunfan@gmail.com>
2020-01-06 20:40:05 -0500

    vpc for application has been initiated

--- Application/infrastructure_wp.tf
+++ Application/infrastructure_wp.tf
@@ -1,7 +1,7 @@
-module "vpc" {
+module "wordpress1" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = var.name
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
