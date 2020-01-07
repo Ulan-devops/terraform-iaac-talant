56d39b2e92832537cb278b155d73c75a4ffffba2
ktalant <uchkunfan@gmail.com>
2020-01-06 20:46:34 -0500

    edited

--- Application/infrastructure_wp.tf
+++ Application/infrastructure_wp.tf
@@ -1,7 +1,7 @@
 module "wordpress1" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = var.name
+  name = var.vpc_name
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
