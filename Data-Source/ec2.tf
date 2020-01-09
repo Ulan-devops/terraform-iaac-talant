b7463bd04f52f7886567b4ed71572e65963e1de3
ktalant <uchkunfan@gmail.com>
2020-01-08 19:51:20 -0500

    new instance has been inititated using ami

--- Data-Source/ec2.tf
+++ Data-Source/ec2.tf
@@ -1,10 +1,20 @@
 provider "aws" {
     region = "us-east-2"
 }
-data "aws_ami" "ubuntu" {
+data "aws_ami" "ubuntu_app1" {
     most_recent = true
     owners = ["099720109477"]
     }
 output "ami" {
     value = "${data.aws_ami.ubuntu.id}"
     }
+
+resource "aws_instance" "app1_vm" {
+  ami           = "${data.aws_ami.ubuntu_app1.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "app1"
+    env  = "dev"
+  }
+}
